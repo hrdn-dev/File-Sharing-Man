39a8b20778045d91c92d1931a9de967a4db302a4
hrdn-dev <112761320+hrdn-dev@users.noreply.github.com>
2022-09-04 02:21:06 +0700

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -1,6 +1,5 @@
-# Credits: @mrismanaziz
-# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
-# t.me/SharingUserbot & t.me/Lunatic0de
+# Credits: @hrdnxxxX
+# t.me/FileTeleForce & t.me/hrdnxxxX
 
 import os
 
